db90ac81f41e4767975cbd95d939e20aa095887b
dev-neo2302 <105268593+mn-1@users.noreply.github.com>
2023-06-21 11:47:19 +0900

    edit

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
+import Link from "next/link";
 
 // ナビゲーション
 const Navigation = () => {
   return (
-    <header className="border-b py-5">
-      <div className="text-center">
-        <Link href="/" className="font-bold text-xl cursor-pointer">
-          FullStackChannel
+    <header className='border-b py-5'>
+      <div className='text-center'>
+        <Link href='/' className='font-bold text-xl cursor-pointer'>
+          Header Sample
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
